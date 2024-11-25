f0d41f8b6f8cf069ba3e3d4b7d86cc66352e189b
ohmycloud <ohmycloudy@gmail.com>
2024-11-25 08:46:27 +0800

    remove anyhow from dev

--- kv-server/src/command.rs
+++ kv-server/src/command.rs
@@ -1,9 +1,11 @@
+use anyhow::Result;
 use course_proto::pb::abi::{CommandResponse, KvPair, Value};
 
-/*
+use crate::error::KvError;
+
 pub trait Storage {
-    fn get(&self, table: &str, key: &str) -> Result<Option<Value, KvError>>;
-    fn set(&self, table: &str, key: String, value: Value) -> Result<Option<Value, KvError>>;
+    fn get(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
+    fn set(&self, table: &str, key: String, value: Value) -> Result<Option<Value>, KvError>;
     fn contains(&self, table: &str, key: &str) -> Result<bool, KvError>;
     fn del(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
     fn get_all(&self, table: &str) -> Result<Vec<KvPair>, KvError>;
@@ -14,4 +16,3 @@ pub trait CommandService {
     /// 处理 Command, 返回 Response
     fn execute(self, store: &impl Storage) -> CommandResponse;
 }
-*/
